94def17b6d3a1d7d6ca02c7b32724037f2da0c9e
k2font <shoichiro.kouno@gmail.com>
2023-06-24 22:52:11 +0900

    melodyのセットアップ

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/olahol/melody"
+)
+
+func main() {
+	m := melody.New()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		m.HandleRequest(w, r)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
+
+	http.ListenAndServe(":5001", nil)
 }

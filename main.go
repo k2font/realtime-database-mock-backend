d2fd72fef9f436296456ae43d1dd5d61afd88c5d
k2font <shoichiro.kouno@gmail.com>
2023-06-24 23:41:05 +0900

    mongoDBの接続処理とデータ追加処理を実装

--- main.go
+++ main.go
@@ -1,16 +1,52 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/olahol/melody"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type Location struct {
+	Name string
+}
+
 func main() {
+	// MongoDBに接続する
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 疎通確認
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
 	m := melody.New()
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		m.HandleRequest(w, r)
+	})
+
+	http.HandleFunc("/close", func(w http.ResponseWriter, r *http.Request) {
+		// DBから接続を切断
+		err = client.Disconnect(
+			context.Background(),
+		)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Connection to MongoDB closed.")
 	})
 
 	// WebSocket接続時の処理
@@ -26,6 +62,15 @@ func main() {
 	// メッセージ受信時の処理
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
 		// DBにデータを登録する
+		collection := client.Database("city").Collection("locations")
+		tokyo := Location{"Tokyo"}
+
+		insertResult, err := collection.InsertOne(context.Background(), tokyo)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Insert a single document: ", insertResult.InsertedID)
 
 		// 登録したらDBからデータを一括取得
 

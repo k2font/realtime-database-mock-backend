7e8496b941b9ba2e7b4b4468cffe087dc6ddb700
k2font <shoichiro.kouno@gmail.com>
2023-07-06 16:47:10 +0900

    バックエンドの処理を少し調整

--- message_handler.go
+++ message_handler.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 
 	"github.com/olahol/melody"
@@ -42,7 +41,6 @@ func MessageHandler(client *mongo.Client, m *melody.Melody) func(s *melody.Sessi
 		case "read":
 			// データをDBから取得
 		case "update":
-			fmt.Println(jsondata)
 			// DB上のデータを更新
 			// 送信されたメッセージのIDを取得し、DB上のデータを更新する
 			filter := bson.D{{Key: "id", Value: jsondata.Data["id"]}}
@@ -57,7 +55,12 @@ func MessageHandler(client *mongo.Client, m *melody.Melody) func(s *melody.Sessi
 			// 何もしない
 		}
 
+		bytes, err := json.Marshal(jsondata)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		// DBに登録されたデータをブロードキャスト
-		m.Broadcast([]byte(jsondata.Data["message"].(string)))
+		m.Broadcast(bytes)
 	}
 }
